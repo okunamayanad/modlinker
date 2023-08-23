0c122519f2b43e5e546bd19ce925bfd6a83fe73b
Baran Mordoğan <baran@okunamayanad.com>
2023-08-23 23:57:39 +0300

    Initial commit

---
+++ background.js
@@ -0,0 +1,21 @@
+console.log("selam ben background");
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request[0] == "checkIfExists") {
+    // if 404 error, return false
+    fetch(request[1], {
+      method: "HEAD",
+      mode: "no-cors"
+    }).then(response => {
+      console.log("response", response);
+      if (response.status == 404) {
+        sendResponse(false);
+      } else {
+        sendResponse(true);
+      }
+    }).catch(err => {
+      console.log("error", err);
+      sendResponse(false);
+    })
+  }
+  return true
+})
